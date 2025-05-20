93bdb0de415c32c3eb3cc8c3b1ee0e613cfaf5a9
The Angriest Crusader <theangriestcrusader@gmail.com>
2025-05-20 19:25:15 +0100

    Add application exit functionality

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CoffeeTime.Interfaces;
+﻿using System.Reactive;
+using Avalonia.Controls.ApplicationLifetimes;
+using CoffeeTime.Interfaces;
 using ReactiveUI;
 
 namespace CoffeeTime.ViewModels;
@@ -14,13 +16,27 @@ public class MainWindowViewModel : ViewModelBase
     #region Public fields and properties
     
     public ReactiveObject? CurrentModuleVm => _currentModuleVm.Value;
+    public ReactiveCommand<Unit, Unit> ExitCommand { get; }
     
     #endregion
 
     public MainWindowViewModel(INavigationService navigationService)
     {
+        ExitCommand = ReactiveCommand.Create(OnExit);
         navigationService
             .CurrentVmChanged
             .ToProperty(this, vm => vm.CurrentModuleVm, out _currentModuleVm);
     }
+    
+    #region Private functions and methods
+    
+    private static void OnExit()
+    {
+        if (Avalonia.Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.Shutdown();
+        }
+    }
+
+    #endregion
 }
